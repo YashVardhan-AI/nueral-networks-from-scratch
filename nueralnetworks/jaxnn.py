7f828cca02e20673c1d9e0e61c41e87cde3a1dc4
porus-creator <workid169@gmail.com>
2021-10-05 20:50:45 +0530

    oof jit is nice

---
+++ nueralnetworks/jaxnn.py
@@ -0,0 +1,61 @@
+from jax import numpy as np
+import jax
+from jax import random
+from functools import partial
+class nn():
+    def __init__(self, X, y, lr = 0.01, epochs = 10000, dims = 2, output_dims = 1):
+        self.X = X
+        self.y = y
+        self.lr = lr
+        self.w1 = random.normal(random.PRNGKey(0), (X.shape[1], dims))
+        self.w2 = random.normal(random.PRNGKey(0), (dims, output_dims)) 
+        self.epochs = epochs    
+    
+    
+    @partial(jax.jit, static_argnums=(0,))
+    def sigmoid(self, x):
+        return 1.0/(1+np.exp(-x))
+    
+   
+
+    def feedforward(self):
+        self.l1 = self.sigmoid(np.dot(self.X, self.w1))
+        self.output = self.sigmoid(np.dot(self.l1, self.w2))
+        
+    @partial(jax.jit, static_argnums=(0,))
+    def sigmoid_derivative(self, x):
+        return x * (1.0 - x)
+        
+    
+ 
+    def backprop(self):
+        
+        
+        # application of the chain rule to find derivative of the loss function with respect to weights2 and weights1
+        d_weights2 = np.dot(self.l1.T, (2*(self.y - self.output) * self.sigmoid_derivative(self.output)))
+        d_weights1 = np.dot(self.X.T,  (np.dot(2*(self.y - self.output) * self.sigmoid_derivative(self.output), self.w2.T) * self.sigmoid_derivative(self.l1)))
+
+        # update the weights with the derivative (slope) of the loss function
+        self.w1 += d_weights1*self.lr
+        self.w2 += d_weights2*self.lr
+                
+    def fit(self):
+        for i in range(self.epochs):
+            self.feedforward()
+            self.backprop()
+            
+    def predict(self):
+        return self.output
+    
+
+if __name__ == "__main__":
+    X = np.array([[0,0,1],
+                  [0,1,1],
+                  [1,0,1],
+                  [1,1,1],
+                  [0,0,0]])
+    y = np.array([[0],[1],[1],[0],[0]])
+    nn = nn(X,y)
+    nn.fit()
+    print(nn.output)
+    
--- nueralnetworks/nnfsbasicnp.py
+++ nueralnetworks/nnfsbasicnp.py
@@ -3,8 +3,8 @@ import numpy as np
 class NeuralNetwork:
     def __init__(self, x, y, learning_rate=0.1):
         self.input      = x
-        self.weights1   = np.random.rand(self.input.shape[1],4) 
-        self.weights2   = np.random.rand(4,1)                 
+        self.weights1   = np.random.randn(self.input.shape[1], 6)
+        self.weights2   = np.random.randn(6,1)                 
         self.y          = y
         self.output     = np.zeros(self.y.shape)
         self.learning_rate = learning_rate
@@ -16,13 +16,13 @@ class NeuralNetwork:
         return x * (1.0 - x)
         
     def feedforward(self):
-        self.layer1 = self.sigmoid(np.dot(self.input, self.weights1))
-        self.output = self.sigmoid(np.dot(self.layer1, self.weights2))
+        self.a1 = self.sigmoid(self.input.dot(self.weights1))
+        self.output = self.sigmoid(self.a1.dot(self.weights2))
 
     def backprop(self):
         # application of the chain rule to find derivative of the loss function with respect to weights2 and weights1
-        d_weights2 = np.dot(self.layer1.T, (2*(self.y - self.output) * self.sigmoid_derivative(self.output)))
-        d_weights1 = np.dot(self.input.T,  (np.dot(2*(self.y - self.output) * self.sigmoid_derivative(self.output), self.weights2.T) * self.sigmoid_derivative(self.layer1)))
+        d_weights2 = np.dot(self.a1.T, (2*(self.y - self.output) * self.sigmoid_derivative(self.output)))
+        d_weights1 = np.dot(self.input.T,  (np.dot(2*(self.y - self.output) * self.sigmoid_derivative(self.output), self.weights2.T) * self.sigmoid_derivative(self.a1)))
 
         # update the weights with the derivative (slope) of the loss function
         self.weights1 += d_weights1*self.learning_rate
@@ -38,9 +38,10 @@ if __name__ == "__main__":
     X = np.array([[0,0,1],
                   [0,1,1],
                   [1,0,1],
-                  [1,1,1]])
-    y = np.array([[0],[1],[1],[0]])
+                  [1,1,1],
+                  [0,0,0]])
+    y = np.array([[0],[1],[1],[0],[0]])
     nn = NeuralNetwork(X,y)
     nn.fit(10000)
     print(nn.output)
-    print(nn.input.shape)
+    
