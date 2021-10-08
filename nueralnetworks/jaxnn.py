51acf0dc03740532e8c89011100270a7e3bbb1b6
porus-creator <workid169@gmail.com>
2021-10-08 11:56:29 +0530

    added bias terms

--- nueralnetworks/jaxnn.py
+++ nueralnetworks/jaxnn.py
@@ -9,6 +9,8 @@ class nn():
         self.lr = lr
         self.w1 = random.normal(random.PRNGKey(0), (X.shape[1], dims))
         self.w2 = random.normal(random.PRNGKey(0), (dims, output_dims)) 
+        self.b1 = random.normal(random.PRNGKey(0), (1, dims))
+        self.b2 = random.normal(random.PRNGKey(0), (1, output_dims))
         
         self.epochs = epochs    
     
@@ -19,8 +21,8 @@ class nn():
 
 
     def feedforward(self):
-        self.l1 = self.sigmoid(np.dot(self.X, self.w1))
-        self.output = self.sigmoid(np.dot(self.l1, self.w2))
+        self.l1 = self.sigmoid(np.dot(self.X, self.w1) + self.b1)
+        self.output = self.sigmoid(np.dot(self.l1, self.w2)+self.b2)
         
     @partial(jax.jit, static_argnums=(0,))
     def sigmoid_derivative(self, x):
@@ -28,14 +30,19 @@ class nn():
         
 
     def backprop(self):
+        
+        delta2 =  (2*(self.y - self.output) * self.sigmoid_derivative(self.output)) 
+        delta1 = np.dot(delta2, self.w2.T) * self.sigmoid_derivative(self.l1)
         # application of the chain rule to find derivative of the loss function with respect to weights2 and weights1
-        d_weights2 = np.dot(self.l1.T, (2*(self.y - self.output) * self.sigmoid_derivative(self.output)))
-        d_weights1 = np.dot(self.X.T,  (np.dot(2*(self.y - self.output) * self.sigmoid_derivative(self.output), self.w2.T) * self.sigmoid_derivative(self.l1)))
+        d_weights2 = np.dot(self.l1.T, delta2)
+        d_weights1 = np.dot(self.X.T,  delta1)
 
         # update the weights with the derivative (slope) of the loss function
         self.w1 += d_weights1*self.lr
         self.w2 += d_weights2*self.lr
-                
+        self.b1 += np.sum(delta2, axis=1, keepdims=True)*self.lr
+        self.b2 += np.sum(delta1, axis=1, keepdims=True)*self.lr
+        
     def fit(self):
         for i in range(self.epochs):
             self.feedforward()
--- nueralnetworks/multiplayerNNFS.py
+++ nueralnetworks/multiplayerNNFS.py
@@ -56,3 +56,4 @@ if __name__ == "__main__":
     nn.fit()
     print(nn.output)
     
+    
